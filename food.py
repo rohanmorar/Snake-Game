363492750c703c178fdb7398bca0dbaf65e9d689
rohanmorar <rohanmorar@gmailc.om>
2022-01-10 17:43:22 -0600

    first commit

---
+++ food.py
@@ -0,0 +1,26 @@
+from turtle import Turtle,Screen
+import random
+
+
+class Food(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.shapesize(0.5)
+        self.penup()
+        self.color("red")
+        x1 = random.randint(-275, 275)
+        y1 = random.randint(-275, 260)
+        self.goto(x1,y1)
+
+    def refresh(self):
+        x = random.randint(-275, 275)
+        y = random.randint(-265, 275)
+        self.goto(x,y)
+
+
+
+
+
+
+
